d6e229b3c458fded6a2eae900e1dfa2ed704dcf0
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-16 09:11:14 +0900

    Update 2023_04_16_090000_create_statuses_table.php

--- database/migrations/2023_04_16_090000_create_statuses_table.php
+++ database/migrations/2023_04_16_090000_create_statuses_table.php
@@ -4,15 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('statuses', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('id');
+
+            $table->foreignId('user_id')
+                  ->cascadeOnDelete()
+                  ->constrained();
+
+            $table->text('content');
+
             $table->timestamps();
         });
     }
