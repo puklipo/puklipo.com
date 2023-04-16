bd174d152a95e6479a973ee9fdce737244648e0f
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-16 15:57:38 +0900

    Update StatusIndex.php

--- app/Http/Livewire/StatusIndex.php
+++ app/Http/Livewire/StatusIndex.php
@@ -5,9 +5,21 @@ namespace App\Http\Livewire;
 use App\Models\Status;
 use Illuminate\View\View;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class StatusIndex extends Component
 {
+    use WithPagination;
+
+    protected $listeners = ['statusCreated' => 'statusCreated'];
+
+    public function statusCreated(): View
+    {
+        $this->resetPage();
+
+        return $this->render();
+    }
+
     public function render(): View
     {
         return view('livewire.status-index')
