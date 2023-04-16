2c809a1bcc8e9518697adc3f087679805e274be7
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-16 09:11:20 +0900

    Update Status.php

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Status extends Model
 {
     use HasFactory;
+    use HasUlids;
+
+    protected $fillable = [
+        'content',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
