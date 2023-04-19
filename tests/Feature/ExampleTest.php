9ed51295982be0f260aef731f2e5b4a306bee557
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-19 11:23:40 +0900

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,27 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $user = User::factory()
+            ->hasStatuses(20)
+            ->create();
 
-        $response->assertStatus(200);
+        $response = $this->actingAs($user)->get('/');
+
+        $response->assertStatus(200)
+            ->assertSeeLivewire('status-form')
+            ->assertSeeLivewire('status-index');
     }
 }
