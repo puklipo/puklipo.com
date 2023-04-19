6f1476d81eb14ba0dec71449859f049afe9e660d
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-19 16:37:45 +0900

    Create NostrCommand.php

---
+++ app/Console/Commands/NostrCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Revolution\Nostr\Facades\Social;
+
+class NostrCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'status:nostr';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $notes = Social::notes(
+            authors: [config('nostr.keys.pk')],
+            since: cache('nostr_since', now()->subDays(7)->timestamp),
+            limit: 50,
+        );
+
+        $user = User::findOrFail(2);
+
+        collect($notes)
+            ->each(fn ($note) => $user->statuses()->updateOrCreate([
+                'nostr_id' => $note['id'],
+            ], [
+                'content' => $note['content'],
+                'created_at' => $note['created_at'],
+            ]));
+
+        cache(['nostr_since' => now()->timestamp]);
+    }
+}
