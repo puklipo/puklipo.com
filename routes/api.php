c9834585a6bd2b7309c519af396f78b141a82e5b
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-20 11:14:53 +0900

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum', 'can:admin'])->post('twitter/webhook', TwitterWebhookController::class);
+Route::middleware('auth:sanctum')
+    ->post('twitter/webhook', TwitterWebhookController::class)
+    ->can('admin');
