f4503b1d61e0ad3d497c4c0fcea7b1cd8ca30bdd
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-22 19:30:19 +0900

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,9 +3,12 @@
 
     <div></div>
 
-    <div class="mt-3">
+    <div class="mt-3 flex flex-row gap-2">
         <a href="https://github.com/pop-culture-studio" class="text-black no-underline inline-flex" target="_blank">
             <x-icon.github/>
+        </a>
+        <a href="{{ url('feed') }}" class="text-black no-underline inline-flex" target="_blank">
+            <x-icon.rss/>
         </a>
     </div>
 </header>
