856bd78f758b1943c1c5d8116d86ce9f0246d171
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-22 19:36:48 +0900

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,13 @@
-<header class="mb-6 p-6 rounded bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700">
+<header class="mb-6 p-6 bg-white dark:bg-gray-800">
     <h1><a href="{{ route('home') }}" class="no-underline">{{ config('app.name') }}</a></h1>
 
     <div></div>
 
     <div class="mt-3 flex flex-row gap-2">
-        <a href="https://github.com/pop-culture-studio" class="text-black no-underline inline-flex" target="_blank">
+        <a href="https://github.com/pop-culture-studio" class="text-black dark:text-white no-underline inline-flex" target="_blank">
             <x-icon.github/>
         </a>
-        <a href="{{ url('feed') }}" class="text-black no-underline inline-flex" target="_blank">
+        <a href="{{ url('feed') }}" class="text-black dark:text-white no-underline inline-flex" target="_blank">
             <x-icon.rss/>
         </a>
     </div>
