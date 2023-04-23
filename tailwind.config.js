e4c030400db13f746a77208eb63756f3c38ecccb
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-23 09:56:31 +0900

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', '"M PLUS Rounded 1c"', ...defaultTheme.fontFamily.sans],
             },
             colors: {
                 indigo: colors.orange
