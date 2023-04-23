193f7cfd8e19c711efc83662d8686d2d6b60fd65
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-23 13:09:36 +0900

    Update item.blade.php

--- resources/views/status/item.blade.php
+++ resources/views/status/item.blade.php
@@ -2,6 +2,9 @@
     <div class="flex justify-between">
         <div class="font-extrabold text-lg inline-flex items-center">
             {{ $status->user->name }}
+            @if($status->user->id === config('puklipo.users.admin'))
+                <x-icon.check class="ml-1 text-indigo-500"/>
+            @endif
         </div>
         <time title="{{ $status->created_at }}">
             <a href="{{ route('status.show', $status) }}"
@@ -17,7 +20,8 @@
 
     @can('admin')
         <div class="flex justify-end">
-            <a href="{{ route('status.edit', $status) }}" class="p-1 no-underline hover:bg-indigo-100 hover:rounded-full">
+            <a href="{{ route('status.edit', $status) }}"
+               class="p-1 no-underline hover:bg-indigo-100 hover:rounded-full">
                 edit
             </a>
         </div>
