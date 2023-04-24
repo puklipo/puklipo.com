db1a6ebb59e494a196f226326c3e863b5f08668d
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-24 10:50:28 +0900

    Create ga.blade.php

---
+++ resources/views/layouts/ga.blade.php
@@ -0,0 +1,11 @@
+@production
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-T2X66R7J4W"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-T2X66R7J4W');
+</script>
+@endproduction
