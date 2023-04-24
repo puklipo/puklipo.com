35a6aa467ab072107a58cdf5034313e67b8a5ca2
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-24 10:29:28 +0900

    Create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="p-3 flex justify-center">
+    <div>
+        <a href="{{ route('home') }}">
+            <x-application-logo class="w-6 h-6 fill-current text-indigo-500 hover:text-indigo-600" />
+        </a>
+    </div>
+</footer>
