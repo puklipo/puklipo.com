d56576cba4aacb24d4264e30aed5912bd2bfc285
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-05-15 14:39:28 +0900

    Update ogp.blade.php

--- resources/views/components/ogp.blade.php
+++ resources/views/components/ogp.blade.php
@@ -1,4 +1,4 @@
 @props(['title', 'description'])
 
-<meta property="og:title" content="{{ $title ?? config('app.name') }}">
+<meta property="og:title" content="{{ trim($title.' '.config('app.name')) }}">
 <meta property="og:description" content="{{ $description ?? '' }}">
