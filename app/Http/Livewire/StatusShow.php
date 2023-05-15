be36d79f15db7d17b6c9d4df663d817852a0dbd1
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-05-15 14:37:12 +0900

    Update StatusShow.php

--- app/Http/Livewire/StatusShow.php
+++ app/Http/Livewire/StatusShow.php
@@ -18,7 +18,7 @@ class StatusShow extends Component
 
     public function render(): View
     {
-        $title = $this->status->created_at;
+        $title = $this->status->created_at.' '.config('app.name');
         $description = str($this->status->content)->replace(PHP_EOL, ' ')->truncate(200)->value();
 
         return view('livewire.status-show')
