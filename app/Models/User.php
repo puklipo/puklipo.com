977be0afb78cb8a46372e7ef05eb6af15a1add63
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-05-15 13:34:39 +0900

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 
     public function statuses(): HasMany
