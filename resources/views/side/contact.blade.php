297d5861a563415cee9e2a63cb3737dcfc3a9fd6
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-07-12 11:56:54 +0900

    Update contact.blade.php

--- resources/views/side/contact.blade.php
+++ resources/views/side/contact.blade.php
@@ -5,8 +5,8 @@
            class="text-indigo-500 font-bold underline">メール</a>で受け付けています。
     </div>
 
-    <h2 class="text-lg text-gray-900 dark:text-white leading-7 font-bold">PCS開発チームについて</h2>
-    <div class="py-3 text-lg">
+    <h3 class="text-md text-gray-900 dark:text-white leading-7 font-bold">PCS開発チームについて</h2>
+    <div class="py-3 text-base">
         就労継続支援B型 ポップカルチャースタジオ未来図（運営会社PayForward）内のプログラマー・エンジニア利用者による開発チーム。B型利用前からフリーランスとして活動していたのでB型スタッフが窓口になり対応し、チームで開発する体制で活動している。
     </div>
 </div>
