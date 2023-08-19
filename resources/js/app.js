a77759238e106bfe1430d1e7cd44fc3b7fb707f1
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-19 10:50:54 +0900

    Add highlight.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,9 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import hljs from 'highlight.js/lib/core';
+import 'highlight.js/styles/devibeans.css'
+import php from 'highlight.js/lib/languages/php';
+hljs.registerLanguage('php', php);
+hljs.highlightAll()
