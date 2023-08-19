b99311105e0b7755ee1885cd944606766a98518d
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-19 10:58:11 +0900

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,5 +9,12 @@ Alpine.start();
 import hljs from 'highlight.js/lib/core';
 import 'highlight.js/styles/devibeans.css'
 import php from 'highlight.js/lib/languages/php';
+
 hljs.registerLanguage('php', php);
 hljs.highlightAll()
+
+document.addEventListener('DOMContentLoaded', () => {
+    Livewire.hook('message.processed', (message, component) => {
+        hljs.highlightAll()
+    })
+})
