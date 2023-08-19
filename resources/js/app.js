56e36cdc30272830e809f2dc121532815c8a5ca9
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-19 11:31:10 +0900

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,15 +6,4 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-import hljs from 'highlight.js/lib/core';
-import 'highlight.js/styles/devibeans.css'
-import php from 'highlight.js/lib/languages/php';
-
-hljs.registerLanguage('php', php);
-hljs.highlightAll()
-
-document.addEventListener('DOMContentLoaded', () => {
-    Livewire.hook('message.processed', (message, component) => {
-        hljs.highlightAll()
-    })
-})
+import './highlight'
