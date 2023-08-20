b99bea4cdc0c0215be4cb2a868ea5c72dee37fb6
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-20 14:00:19 +0900

    Update develop.blade.php

--- resources/views/menu/develop.blade.php
+++ resources/views/menu/develop.blade.php
@@ -4,7 +4,7 @@
     <div class="mt-1">
         Webシステム開発とその後の継続的な運用。公開用に限らず社内専用システムやスマホアプリのサーバーサイドまで。
         <div class="bg-white dark:bg-gray-800 dark:text-white border rounded my-2">
-            <div class="border-b p-2"><span class="font-bold mr-3">料金目安</span>月額22万円(20万円+消費税2万円)</div>
+            <div class="border-b p-2"><span class="font-bold mr-3">料金目安</span>要見積。月額制。</div>
             <div class="border-b p-2"><span class="font-bold mr-3">営業日</span>月曜から土曜</div>
             <div class="p-2"><span class="font-bold mr-3">営業時間</span>10時から15時</div>
         </div>
