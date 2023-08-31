e110b1b01df555774c08b2b84d3fb225df842948
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-31 09:20:02 +0900

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,6 +22,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('admin', fn(User $user) => $user->id === config('puklipo.users.admin'));
+        Gate::define('admin', fn (User $user) => $user->id === config('puklipo.users.admin'));
     }
 }
