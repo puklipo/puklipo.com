8b220a376a0ac3e59ea954f8540137f7fc57ac4f
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-31 09:57:40 +0900

    Update item.blade.php

--- resources/views/status/item.blade.php
+++ resources/views/status/item.blade.php
@@ -1,4 +1,4 @@
-<div class="p-3 border-b border-gray-200 dark:border-gray-500 hover:bg-indigo-50 dark:hover:bg-gray-800">
+<div class="p-3 border-b border-gray-200 dark:border-gray-500 hover:bg-indigo-50 dark:hover:bg-gray-800" wire:key="{{ $status->id }}">
     <div class="flex justify-between">
         <div class="font-extrabold text-lg inline-flex items-center">
             {{ $status->user->name }}
