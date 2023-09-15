7ccb4f055491e4588b7b375bbc5f647f0d4b59cb
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-09-15 13:47:43 +0900

    Update status-index.blade.php

--- resources/views/livewire/status-index.blade.php
+++ resources/views/livewire/status-index.blade.php
@@ -6,12 +6,12 @@
     <div x-data @page-updated.window="$el.scrollIntoView({behavior: 'smooth'})">
         <livewire:status-filter></livewire:status-filter>
 
-        @foreach($statuses as $status)
+        @foreach($this->statuses as $status)
             @include('status.item')
         @endforeach
 
         <p>
-            {{ $statuses->links() }}
+            {{ $this->statuses->links() }}
         </p>
     </div>
 </div>
