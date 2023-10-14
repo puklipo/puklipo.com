01b24655afadf55028811b466ae01956d21399cc
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-10-14 09:53:38 +0900

    Update menu.blade.php

--- resources/views/side/menu.blade.php
+++ resources/views/side/menu.blade.php
@@ -1,5 +1,5 @@
 <div class="p-3 text-sm text-gray-700 dark:text-white bg-gray-200 dark:bg-gray-700">
-    <h2 class="text-lg text-gray-900 dark:text-white leading-7 font-bold">仕事メニュー</h3>
+    <h2 class="text-lg text-gray-900 dark:text-white leading-7 font-bold">仕事メニュー</h2>
     <div class="divide-y">
         @includeIf('menu.develop')
         @includeIf('menu.laravel')
