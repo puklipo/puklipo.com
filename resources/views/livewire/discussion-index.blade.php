2f0e055fc7e0019240a664d64d963bf5988f8341
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-10-14 12:24:04 +0900

    wip

--- resources/views/livewire/discussion-index.blade.php
+++ resources/views/livewire/discussion-index.blade.php
@@ -7,21 +7,7 @@
 
     <div class="mx-3 px-3" x-data @page-updated.window="$el.scrollIntoView({behavior: 'smooth'})">
         @forelse($this->discussions as $discussion)
-            <div>
-                <h2>
-                    @if($discussion->private)
-                        <x-icon.lock-closed class="inline-flex"></x-icon.lock-closed>
-                    @endif
-                    <span class="font-normal">[{{ $discussion->version }}]</span>
-                    <a href="{{ route('discussion.show', $discussion) }}"
-                       class="no-underline hover:underline">{{ $discussion->title }}</a>
-                </h2>
-                <div>
-                    <span class="font-bold">{{ $discussion->user->name ?? '匿名' }}</span>
-                    <time class="text-gray-400" datetime="{{ $discussion->created_at }}" title="{{ $discussion->created_at }}">{{ $discussion->created_at->diffForHumans() }}</time>
-                </div>
-
-            </div>
+            @include('discussions.discussion')
         @empty
             質問はありません。
         @endforelse
--- resources/views/livewire/discussion-private.blade.php
+++ resources/views/livewire/discussion-private.blade.php
@@ -2,23 +2,11 @@
     @include('discussions.nav')
 
     <div class="mx-3 px-3" x-data @page-updated.window="$el.scrollIntoView({behavior: 'smooth'})">
-        @foreach($this->discussions as $discussion)
-            <div>
-                <h2>
-                    @if($discussion->private)
-                        <x-icon.lock-closed class="inline-flex"></x-icon.lock-closed>
-                    @endif
-                    <span class="font-normal">[{{ $discussion->version }}]</span>
-                    <a href="{{ route('discussion.show', $discussion) }}"
-                       class="no-underline hover:underline">{{ $discussion->title }}</a>
-                </h2>
-                <div>
-                    <span class="font-bold">{{ $discussion->user->name ?? '匿名' }}</span>
-                    <time class="text-gray-400">{{ $discussion->created_at }}</time>
-                </div>
-
-            </div>
-        @endforeach
+        @forelse($this->discussions as $discussion)
+            @include('discussions.discussion')
+        @empty
+            質問はありません。
+        @endforelse
 
         <p>
             {{ $this->discussions->links() }}
