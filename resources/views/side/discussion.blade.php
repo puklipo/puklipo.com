3caaacf5414d0b5dd2512a5ce37e146c04e10e2a
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-10-14 11:11:08 +0900

    Update discussion.blade.php

--- resources/views/side/discussion.blade.php
+++ resources/views/side/discussion.blade.php
@@ -1,3 +1,5 @@
 <div class="p-3 text-md text-white bg-indigo-400 dark:bg-gray-700">
-    <h2><a href="{{ route('discussion') }}">Laravel専用相談所<span class="ml-1 px-1 text-indigo-500 bg-white rounded-sm">{{ \App\Models\Discussion::withoutPrivate()->count() }}</span></a></h2>
+    <h2><a href="{{ route('discussion') }}">Laravel専用相談所<span
+                class="ml-1 px-1 text-indigo-500 bg-white rounded-sm">{{ cache()->remember('side.discussion.count', now()->addHour(), fn() => \App\Models\Discussion::withoutPrivate()->count()) }}</span></a>
+    </h2>
 </div>
