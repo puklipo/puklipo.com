99c840478dd8176651565f89a3200725e13cd3fa
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-10-14 12:42:05 +0900

    Update nav.blade.php

--- resources/views/discussions/nav.blade.php
+++ resources/views/discussions/nav.blade.php
@@ -1,9 +1,10 @@
 @auth
-    <div class="mx-6 px-6 grid grid-flow-col auto-cols-max gap-4">
-        <span><a href="{{ route('discussion') }}">公開質問</a></span>
-        <span><a href="{{ route('discussion.my') }}">自分の質問</a></span>
+    <div class="mx-3 px-3 text-center grid grid-flow-col auto-cols-max gap-4">
+        <span><a href="{{ route('discussion') }}" @unless(request()->routeIs('discussion')) class="no-underline" @endunless>公開質問</a></span>
+        <span><a href="{{ route('discussion.my') }}" @unless(request()->routeIs('discussion.my')) class="no-underline" @endunless>自分の質問</a></span>
         @can('admin')
-            <span><a href="{{ route('discussion.private') }}">非公開質問</a></span>
+            <span><a href="{{ route('discussion.private') }}"
+                     @unless(request()->routeIs('discussion.private')) class="no-underline" @endunless>非公開質問</a></span>
         @endcan
     </div>
 @endauth
