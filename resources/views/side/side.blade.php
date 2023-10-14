e91705e4d1ae188311f6c9e5e8208b035f9df516
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-10-14 10:03:49 +0900

    Update side.blade.php

--- resources/views/side/side.blade.php
+++ resources/views/side/side.blade.php
@@ -1,8 +1,4 @@
-<div class="p-3 text-md text-white bg-indigo-400 ">
-    <h2><a href="{{ route('discussion') }}">Laravel専用相談所</a></h2>
-</div>
-
-
+@include('side.discussion')
 @include('side.contact')
 @include('side.menu')
 @include('side.books')
