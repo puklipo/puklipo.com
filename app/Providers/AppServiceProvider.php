9a0f4c647f34739c297e098f07829b6c61590006
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-10-22 15:08:49 +0900

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Discussion;
 use App\Models\Status;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
 
         Relation::morphMap([
             'status' => Status::class,
+            'discussion' => Discussion::class,
         ]);
     }
 }
