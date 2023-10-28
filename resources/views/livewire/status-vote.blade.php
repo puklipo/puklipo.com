b366a8dda6dbf4e3e09fc512b1d6848d12f332ad
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-10-28 14:04:33 +0900

    vote

--- resources/views/livewire/status-vote.blade.php
+++ resources/views/livewire/status-vote.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <x-secondary-button wire:click="up">正しい {{ cache()->get('status:up:'.$status->id, 0) }}</x-secondary-button>
-    <x-secondary-button wire:click="down">間違い {{ cache()->get('status:down:'.$status->id, 0) }}</x-secondary-button>
+    <x-secondary-button wire:click="up"><x-icon.up class="mr-1"></x-icon.up> 正しい {{ cache()->get('status:up:'.$status->id, 0) }}</x-secondary-button>
+    <x-secondary-button wire:click="down"><x-icon.down class="mr-1"></x-icon.down> 間違い {{ cache()->get('status:down:'.$status->id, 0) }}</x-secondary-button>
 </div>
