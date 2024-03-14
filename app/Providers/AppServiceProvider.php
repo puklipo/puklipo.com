98c5ee44eb932a4cd45c45c71047531278ac0719
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-03-14 11:00:48 +0900

    admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Models\Discussion;
 use App\Models\Status;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,5 +31,7 @@ class AppServiceProvider extends ServiceProvider
             'status' => Status::class,
             'discussion' => Discussion::class,
         ]);
+
+        Gate::define('admin', fn (User $user) => $user->id === config('puklipo.users.admin'));
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Discussion;
-use App\Models\User;
-use App\Policies\DiscussionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,6 +20,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('admin', fn (User $user) => $user->id === config('puklipo.users.admin'));
+        //
     }
 }
