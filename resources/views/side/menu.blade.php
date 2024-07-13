8d57420c658b9df49819c2ec6e0a63aa187fdc09
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-07-13 11:01:38 +0900

    wip

--- resources/views/side/menu.blade.php
+++ resources/views/side/menu.blade.php
@@ -3,13 +3,7 @@
 
     <div class="mt-1">
         <div class="bg-white dark:bg-gray-800 dark:text-white border rounded my-2">
-            <div class="p-2"><span class="font-bold mr-3">料金目安</span>月額20～100万円（税別）インボイス対応</div>
+            <div class="p-2">新規受注は縮小中</div>
         </div>
-    </div>
-
-    <div class="divide-y">
-        @includeIf('menu.develop')
-        @includeIf('menu.laravel')
-        @includeIf('menu.remote')
     </div>
 </div>
