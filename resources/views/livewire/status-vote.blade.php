8e8350985a53451e428ac2a34d90c46c3204da5f
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-09-06 11:07:48 +0900

    Update status-vote.blade.php

--- resources/views/livewire/status-vote.blade.php
+++ resources/views/livewire/status-vote.blade.php
@@ -1,4 +1,15 @@
-<div>
-    <x-secondary-button wire:click="up"><x-icon.up class="mr-1"></x-icon.up> 正しい {{ cache()->get('status:up:'.$status->id, 0) }}</x-secondary-button>
-    <x-secondary-button wire:click="down"><x-icon.down class="mr-1"></x-icon.down> 間違い {{ cache()->get('status:down:'.$status->id, 0) }}</x-secondary-button>
+<div class="px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md shadow-sm  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800">
+    <div>
+        <x-secondary-button wire:click="up">
+            <x-icon.up class="mr-1"></x-icon.up>
+            正しい {{ cache()->get('status:up:'.$status->id, 0) }}</x-secondary-button>
+        <x-secondary-button wire:click="down">
+            <x-icon.down class="mr-1"></x-icon.down>
+            間違い {{ cache()->get('status:down:'.$status->id, 0) }}</x-secondary-button>
+    </div>
+
+    <div class="mt-3 text-sm text-gray-700 dark:text-gray-300 tracking-widest">
+        Laravel Tips botの投稿はOpenAI APIの出力です。当初はTipsを投稿させていましたが間違いが多すぎるので現在はリリースノートの日本語訳が主。
+    </div>
+
 </div>
