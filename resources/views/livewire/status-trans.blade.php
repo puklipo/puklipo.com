abb020749e99f65dc72bcb3d2c79c5e23984b7b5
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-09-07 08:44:18 +0900

    Update status-trans.blade.php

--- resources/views/livewire/status-trans.blade.php
+++ resources/views/livewire/status-trans.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="mt-3">
     <x-secondary-button wire:click="trans" class="ml-3" title="{{ __('翻訳') }}" wire:loading.attr="disabled">
         {{ __('翻訳') }}
     </x-secondary-button>
