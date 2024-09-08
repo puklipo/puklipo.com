c7b86776e4450cf88c510ec87b886a8c600a8592
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-09-08 14:13:14 +0900

    Update contact.blade.php

--- resources/views/side/contact.blade.php
+++ resources/views/side/contact.blade.php
@@ -1,7 +1,15 @@
 <div class="p-3 text-sm text-gray-700 dark:text-white bg-gray-300 dark:bg-gray-800">
     <h2 class="text-lg text-gray-900 dark:text-white leading-7 font-bold">Contact</h2>
     <div class="py-3 text-lg">
-        <a href="mailto:pcs.engineer.team@gmail.com"
-           class="text-indigo-500 font-bold underline">email</a>
+        <ul>
+            <li>
+                <a href="mailto:pcs.engineer.team@gmail.com"
+                   class="text-indigo-500 font-bold underline">e-mail</a>
+            </li>
+            <li>
+                <a href="https://github.com/pop-culture-studio"
+                   class="text-indigo-500 font-bold underline">GitHub</a>
+            </li>
+        </ul>
     </div>
 </div>
