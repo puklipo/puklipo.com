919c4d9c4919463a13ad55e6a7ce8f501e7c821a
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-09-08 13:56:44 +0900

    wip

--- resources/views/side/contact.blade.php
+++ resources/views/side/contact.blade.php
@@ -4,9 +4,4 @@
         <a href="mailto:pcs.engineer.team@gmail.com"
            class="text-indigo-500 font-bold underline">email</a>
     </div>
-
-    <h3 class="text-base text-gray-900 dark:text-white leading-7 font-bold">PCS開発チームについて</h3>
-    <div class="py-3 text-sm">
-        ポップカルチャースタジオ未来図（運営 株式会社PayForward 法人番号1290001091513）内のプログラマー・エンジニア利用者による開発チーム。
-    </div>
 </div>
