4fe6d016fbf047176ce59451ca8875ca5b6f233b
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-10-03 11:31:10 +0900

    Update articles.php

--- config/articles.php
+++ config/articles.php
@@ -3,19 +3,19 @@
 return [
     [
         'title' => 'Laravel Reverbのサンプルプロジェクト',
-        'url' => 'https://github.com/pop-culture-studio/laravel-reverb-sample',
+        'url' => 'https://github.com/puklipo/laravel-reverb-sample',
     ],
     [
         'title' => 'Laravel11 ドキュメントに書いてないbootstrap/app.phpの使い方',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/laravel11-application-builder.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/laravel11-application-builder.md',
     ],
     [
         'title' => 'Laravel Sanctum APIトークン認証のサンプル',
-        'url' => 'https://github.com/pop-culture-studio/sanctum-api-token-sample',
+        'url' => 'https://github.com/puklipo/sanctum-api-token-sample',
     ],
     [
         'title' => 'Laravel11 フォロー・フォロワーのリレーションのサンプル',
-        'url' => 'https://github.com/pop-culture-studio/follow-sample',
+        'url' => 'https://github.com/puklipo/follow-sample',
     ],
     [
         'title' => 'Laravel11へのアップグレード（スリム化版）',
@@ -31,7 +31,7 @@ return [
     ],
     [
         'title' => 'Livewireの普通の使い方とVoltの比較',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/livewire-volt.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/livewire-volt.md',
     ],
     [
         'title' => 'Laravel Collectiveが終了する',
@@ -43,42 +43,42 @@ return [
     ],
     [
         'title' => 'LaravelはMVCではない',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/laravel-is-not-mvc.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-is-not-mvc.md',
     ],
     [
         'title' => 'Laravel 開発環境構築 Mac版',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/laravel-installation-mac.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-installation-mac.md',
     ],
     [
         'title' => 'Laravel 新PCでの開発環境構築 Windows11版',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/laravel-installation-windows.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-installation-windows.md',
     ],
     [
         'title' => 'Laravelの禁忌',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/laravel-taboo.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-taboo.md',
     ],
     [
         'title' => 'Vercel 外部WebAPIから取得したjsonをそのまま返すサーバーレス関数 (Node.js/TypeScript/Golang/Ruby/Python版)',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/vercel-severless-functions-api-json.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/vercel-severless-functions-api-json.md',
     ],
     [
         'title' => 'Laravel mixからlaravel-vite-pluginに移行する',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/laravel-vite-plugin.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-vite-plugin.md',
     ],
     [
         'title' => 'LaravelでreCAPTCHA',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/laravel-recaptcha.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-recaptcha.md',
     ],
     [
         'title' => 'PHPにはunlessがないのでLaravelが用意している類似機能が地味に役に立つ',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/laravel-unless.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-unless.md',
     ],
     [
         'title' => 'Laravel/Livewire ページネーションでページ移動後に自動スクロールさせる',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/livewire-pagination.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/livewire-pagination.md',
     ],
     [
         'title' => 'Octane環境下ではLaravelとLivewireのページネーションを併用しにくい',
-        'url' => 'https://github.com/pop-culture-studio/articles/blob/main/livewire-paginators-with-octane.md',
+        'url' => 'https://github.com/puklipo/articles/blob/main/livewire-paginators-with-octane.md',
     ],
 ];
