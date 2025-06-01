8dade0c7647d3a658e0eab5c20e307b98b9d9065
puklipo <pcs.engineer.team@gmail.com>
2025-06-01 11:45:00 +0900

    Update articles.php

--- config/articles.php
+++ config/articles.php
@@ -17,34 +17,34 @@ return [
         'title' => 'Laravel11 フォロー・フォロワーのリレーションのサンプル',
         'url' => 'https://github.com/puklipo/follow-sample',
     ],
-//    [
-//        'title' => 'Laravel11へのアップグレード（スリム化版）',
-//        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-upgrade-slim',
-//    ],
-//    [
-//        'title' => 'Laravel11以降から使い始めた人に必要そうな質問と回答一覧',
-//        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-faq',
-//    ],
-//    [
-//        'title' => 'Laravel11以降の新常識',
-//        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-new-structure',
-//    ],
+    //    [
+    //        'title' => 'Laravel11へのアップグレード（スリム化版）',
+    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-upgrade-slim',
+    //    ],
+    //    [
+    //        'title' => 'Laravel11以降から使い始めた人に必要そうな質問と回答一覧',
+    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-faq',
+    //    ],
+    //    [
+    //        'title' => 'Laravel11以降の新常識',
+    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-new-structure',
+    //    ],
     [
         'title' => 'Livewireの普通の使い方とVoltの比較',
         'url' => 'https://github.com/puklipo/articles/blob/main/livewire-volt.md',
     ],
-//    [
-//        'title' => 'Laravel Collectiveが終了する',
-//        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel-collective',
-//    ],
-//    [
-//        'title' => 'フロントエンドのような開発体験が可能になるLaravel',
-//        'url' => 'https://zenn.dev/pcs_engineer/articles/laracon-us-2023',
-//    ],
-//    [
-//        'title' => 'LaravelはMVCではない',
-//        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-is-not-mvc.md',
-//    ],
+    //    [
+    //        'title' => 'Laravel Collectiveが終了する',
+    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel-collective',
+    //    ],
+    //    [
+    //        'title' => 'フロントエンドのような開発体験が可能になるLaravel',
+    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laracon-us-2023',
+    //    ],
+    //    [
+    //        'title' => 'LaravelはMVCではない',
+    //        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-is-not-mvc.md',
+    //    ],
     [
         'title' => 'Laravel 開発環境構築 Mac版',
         'url' => 'https://github.com/puklipo/articles/blob/main/laravel-installation-mac.md',
@@ -53,22 +53,22 @@ return [
         'title' => 'Laravel 新PCでの開発環境構築 Windows11版',
         'url' => 'https://github.com/puklipo/articles/blob/main/laravel-installation-windows.md',
     ],
-//    [
-//        'title' => 'Laravelの禁忌',
-//        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-taboo.md',
-//    ],
-//    [
-//        'title' => 'Vercel 外部WebAPIから取得したjsonをそのまま返すサーバーレス関数 (Node.js/TypeScript/Golang/Ruby/Python版)',
-//        'url' => 'https://github.com/puklipo/articles/blob/main/vercel-severless-functions-api-json.md',
-//    ],
-//    [
-//        'title' => 'Laravel mixからlaravel-vite-pluginに移行する',
-//        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-vite-plugin.md',
-//    ],
-//    [
-//        'title' => 'LaravelでreCAPTCHA',
-//        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-recaptcha.md',
-//    ],
+    //    [
+    //        'title' => 'Laravelの禁忌',
+    //        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-taboo.md',
+    //    ],
+    //    [
+    //        'title' => 'Vercel 外部WebAPIから取得したjsonをそのまま返すサーバーレス関数 (Node.js/TypeScript/Golang/Ruby/Python版)',
+    //        'url' => 'https://github.com/puklipo/articles/blob/main/vercel-severless-functions-api-json.md',
+    //    ],
+    //    [
+    //        'title' => 'Laravel mixからlaravel-vite-pluginに移行する',
+    //        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-vite-plugin.md',
+    //    ],
+    //    [
+    //        'title' => 'LaravelでreCAPTCHA',
+    //        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-recaptcha.md',
+    //    ],
     [
         'title' => 'PHPにはunlessがないのでLaravelが用意している類似機能が地味に役に立つ',
         'url' => 'https://github.com/puklipo/articles/blob/main/laravel-unless.md',
