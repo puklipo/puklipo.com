610899a0af55632523bef1733e841d82832dc53f
puklipo <pcs.engineer.team@gmail.com>
2025-06-02 18:38:33 +0900

    Update articles.php

--- config/articles.php
+++ config/articles.php
@@ -2,16 +2,16 @@
 
 return [
     [
-        'title' => 'Laravel Reverbのサンプルプロジェクト',
-        'url' => 'https://github.com/puklipo/laravel-reverb-sample',
+        'title' => 'Laravel 11 Reverb Sample',
+        'url' => 'https://github.com/orgs/invokable/discussions/14',
     ],
     [
-        'title' => 'Laravel11 ドキュメントに書いてないbootstrap/app.phpの使い方',
-        'url' => 'https://github.com/puklipo/articles/blob/main/laravel11-application-builder.md',
+        'title' => 'Laravel 11: Undocumented Ways to Use bootstrap/app.php',
+        'url' => 'https://github.com/orgs/invokable/discussions/6',
     ],
     [
-        'title' => 'Laravel Sanctum APIトークン認証のサンプル',
-        'url' => 'https://github.com/puklipo/sanctum-api-token-sample',
+        'title' => 'Laravel 11 Sanctum API Token Authentication Sample',
+        'url' => 'https://github.com/orgs/invokable/discussions/12',
     ],
     [
         'title' => 'Laravel11 フォロー・フォロワーのリレーションのサンプル',
@@ -30,21 +30,9 @@ return [
     //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-new-structure',
     //    ],
     [
-        'title' => 'Livewireの普通の使い方とVoltの比較',
-        'url' => 'https://github.com/puklipo/articles/blob/main/livewire-volt.md',
+        'title' => 'Comparison of Standard Livewire Usage and Volt',
+        'url' => 'https://github.com/orgs/invokable/discussions/7',
     ],
-    //    [
-    //        'title' => 'Laravel Collectiveが終了する',
-    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel-collective',
-    //    ],
-    //    [
-    //        'title' => 'フロントエンドのような開発体験が可能になるLaravel',
-    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laracon-us-2023',
-    //    ],
-    //    [
-    //        'title' => 'LaravelはMVCではない',
-    //        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-is-not-mvc.md',
-    //    ],
     [
         'title' => 'Laravel 開発環境構築 Mac版',
         'url' => 'https://github.com/puklipo/articles/blob/main/laravel-installation-mac.md',
@@ -54,31 +42,12 @@ return [
         'url' => 'https://github.com/puklipo/articles/blob/main/laravel-installation-windows.md',
     ],
     //    [
-    //        'title' => 'Laravelの禁忌',
-    //        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-taboo.md',
-    //    ],
-    //    [
-    //        'title' => 'Vercel 外部WebAPIから取得したjsonをそのまま返すサーバーレス関数 (Node.js/TypeScript/Golang/Ruby/Python版)',
-    //        'url' => 'https://github.com/puklipo/articles/blob/main/vercel-severless-functions-api-json.md',
-    //    ],
-    //    [
-    //        'title' => 'Laravel mixからlaravel-vite-pluginに移行する',
-    //        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-vite-plugin.md',
-    //    ],
-    //    [
-    //        'title' => 'LaravelでreCAPTCHA',
-    //        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-recaptcha.md',
+    //        'title' => 'Migrating from Laravel Mix to laravel-vite-plugin',
+    //        'url' => 'https://github.com/orgs/invokable/discussions/8',
     //    ],
     [
-        'title' => 'PHPにはunlessがないのでLaravelが用意している類似機能が地味に役に立つ',
+        'title' => 'PHP Lacks unless, so Laravel\'s Similar Features are Subtly Useful',
         'url' => 'https://github.com/puklipo/articles/blob/main/laravel-unless.md',
-    ],
-    [
-        'title' => 'Laravel/Livewire ページネーションでページ移動後に自動スクロールさせる',
-        'url' => 'https://github.com/puklipo/articles/blob/main/livewire-pagination.md',
-    ],
-    [
-        'title' => 'Octane環境下ではLaravelとLivewireのページネーションを併用しにくい',
-        'url' => 'https://github.com/puklipo/articles/blob/main/livewire-paginators-with-octane.md',
+    ],url' => 'https://github.com/puklipo/articles/blob/main/livewire-paginators-with-octane.md',
     ],
 ];
