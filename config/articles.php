5bc8bfc6752892a1459025326f9b91a6e3ee151f
puklipo <pcs.engineer.team@gmail.com>
2025-06-02 18:52:55 +0900

    Update articles.php

--- config/articles.php
+++ config/articles.php
@@ -13,33 +13,21 @@ return [
         'title' => 'Laravel 11 Sanctum API Token Authentication Sample',
         'url' => 'https://github.com/orgs/invokable/discussions/12',
     ],
-    [
-        'title' => 'Laravel11 フォロー・フォロワーのリレーションのサンプル',
-        'url' => 'https://github.com/puklipo/follow-sample',
-    ],
     //    [
-    //        'title' => 'Laravel11へのアップグレード（スリム化版）',
-    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-upgrade-slim',
+    //        'title' => 'Upgrading to Laravel 11 (Slim Version)',
+    //        'url' => 'https://github.com/orgs/invokable/discussions/16',
     //    ],
     //    [
-    //        'title' => 'Laravel11以降から使い始めた人に必要そうな質問と回答一覧',
-    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-faq',
+    //        'title' => 'FAQ for those starting with Laravel 11 and later',
+    //        'url' => 'https://github.com/orgs/invokable/discussions/17',
     //    ],
     //    [
-    //        'title' => 'Laravel11以降の新常識',
-    //        'url' => 'https://zenn.dev/pcs_engineer/articles/laravel11-new-structure',
+    //        'title' => 'New Norms Since Laravel 11',
+    //        'url' => 'https://github.com/orgs/invokable/discussions/15',
     //    ],
     [
         'title' => 'Comparison of Standard Livewire Usage and Volt',
         'url' => 'https://github.com/orgs/invokable/discussions/7',
-    ],
-    [
-        'title' => 'Laravel 開発環境構築 Mac版',
-        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-installation-mac.md',
-    ],
-    [
-        'title' => 'Laravel 新PCでの開発環境構築 Windows11版',
-        'url' => 'https://github.com/puklipo/articles/blob/main/laravel-installation-windows.md',
     ],
     //    [
     //        'title' => 'Migrating from Laravel Mix to laravel-vite-plugin',
