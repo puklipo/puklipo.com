48a8e96fd2ca4970a565f32d05e9fec39c44e0ae
puklipo <pcs.engineer.team@gmail.com>
2025-06-02 18:53:41 +0900

    Update articles.php

--- config/articles.php
+++ config/articles.php
@@ -13,26 +13,26 @@ return [
         'title' => 'Laravel 11 Sanctum API Token Authentication Sample',
         'url' => 'https://github.com/orgs/invokable/discussions/12',
     ],
-    //    [
-    //        'title' => 'Upgrading to Laravel 11 (Slim Version)',
-    //        'url' => 'https://github.com/orgs/invokable/discussions/16',
-    //    ],
-    //    [
-    //        'title' => 'FAQ for those starting with Laravel 11 and later',
-    //        'url' => 'https://github.com/orgs/invokable/discussions/17',
-    //    ],
-    //    [
-    //        'title' => 'New Norms Since Laravel 11',
-    //        'url' => 'https://github.com/orgs/invokable/discussions/15',
-    //    ],
+    [
+       'title' => 'Upgrading to Laravel 11 (Slim Version)',
+       'url' => 'https://github.com/orgs/invokable/discussions/16',
+    ],
+    [
+       'title' => 'FAQ for those starting with Laravel 11 and later',
+        'url' => 'https://github.com/orgs/invokable/discussions/17',
+    ],
+    [
+       'title' => 'New Norms Since Laravel 11',
+       'url' => 'https://github.com/orgs/invokable/discussions/15',
+    ],
     [
         'title' => 'Comparison of Standard Livewire Usage and Volt',
         'url' => 'https://github.com/orgs/invokable/discussions/7',
     ],
-    //    [
-    //        'title' => 'Migrating from Laravel Mix to laravel-vite-plugin',
-    //        'url' => 'https://github.com/orgs/invokable/discussions/8',
-    //    ],
+    [
+        'title' => 'Migrating from Laravel Mix to laravel-vite-plugin',
+        'url' => 'https://github.com/orgs/invokable/discussions/8',
+    ],
     [
         'title' => 'PHP Lacks unless, so Laravel\'s Similar Features are Subtly Useful',
         'url' => 'https://github.com/puklipo/articles/blob/main/laravel-unless.md',
